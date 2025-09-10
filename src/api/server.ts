0fd7607635af1e9c6cd4b17e7c13e93d4255f382
alienaudo <contatotiago819@gmail.com>
2025-09-10 10:04:43 -0400

    Code Improvements
    - Adds secret and hook to cookie plugin
    - Adds line breaks
    - Removes old messaging plugin

--- src/api/server.ts
+++ src/api/server.ts
@@ -4,13 +4,13 @@ import cookie from "@fastify/cookie";
 import { PrismaClient } from "@prisma/client";
 import multipart from '@fastify/multipart';
 import fastifyRateLimit from "@fastify/rate-limit";
-import { MessageRoutPlugin } from "../services/Message/route/Message.Route.js";
 import { FastifyInstance } from "fastify/types/instance.js";
 import { ChannelRoutePlugin } from "../services/Channel/route/Channel.Route.js";
 import { FastifyRequest } from "fastify/types/request.js";
-import { FastifyReply } from "fastify/types/reply.js";
+import { FastifyReply, ReplyGenericInterface } from "fastify/types/reply.js";
 import { StatusCodes } from "http-status-codes/build/es/status-codes.js";
 import { ReasonPhrases } from "http-status-codes/build/es/reason-phrases.js";
+import "dotenv/config.js";
 
 export function buildApp(prismaClient: PrismaClient): FastifyInstance {
 
@@ -20,7 +20,13 @@ export function buildApp(prismaClient: PrismaClient): FastifyInstance {
 
     });
 
-    app.setErrorHandler((error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
+    app.setErrorHandler((
+
+        error: FastifyError,
+        _request: FastifyRequest,
+        reply: FastifyReply
+
+    ): FastifyReply<ReplyGenericInterface> => {
 
         app.log.error(error);
 
@@ -33,7 +39,12 @@ export function buildApp(prismaClient: PrismaClient): FastifyInstance {
 
     });
 
-    app.register(cookie);
+    app.register(cookie, {
+
+        secret: process.env.JWT_SECRET,
+        hook: "preHandler"
+
+    });
 
     app.register(fastifyRateLimit, {
 
@@ -48,12 +59,6 @@ export function buildApp(prismaClient: PrismaClient): FastifyInstance {
     app.register(authRoutesPlugin, {
 
         prefix: '/api/auth'
-
-    });
-
-    app.register(MessageRoutPlugin, {
-
-        prefix: '/api/message'
 
     });
 
