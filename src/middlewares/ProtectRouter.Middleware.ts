c8bc811a3cb8deb17f130132da1c98edc8a2ccd3
alienaudo <contatotiago819@gmail.com>
2025-09-10 09:57:19 -0400

    Transform Middleware into a Class
    To better implement dependency injection

--- src/middlewares/ProtectRouter.Middleware.ts
+++ src/middlewares/ProtectRouter.Middleware.ts
@@ -1,13 +1,13 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../lib/prisma.js";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { UserProtectRouter } from "../interfaces/UserProtectRoute.Interface.js";
 import { FastifyRequest } from "fastify/types/request.js";
 import { FastifyReply } from "fastify/types/reply.js";
 import { DoneFuncWithErrOrRes } from "fastify";
-import { ProtectRouter } from "../interfaces/ProtectRouter.Interface.js";
 import { StatusCodes } from "http-status-codes/build/es/status-codes.js";
 import { ReasonPhrases } from "http-status-codes/build/es/reason-phrases.js";
+import { PrismaClient } from "@prisma/client";
+import { ProtectRouter } from "../interfaces/ProtectRouter.Interface.js";
 
 declare module 'fastify' {
 
@@ -19,102 +19,140 @@ declare module 'fastify' {
 
 };
 
-const jwtSecret: string | undefined = process.env.JWT_SECRET;
+export class ProtectMiddleware {
 
-if (!jwtSecret) {
+    private readonly jwtSecret: string | undefined = process.env.JWT_SECRET;
 
-    throw new Error('Ambiente variable "JWT_SECRET" is not set');
+    private readonly prisma: PrismaClient;
 
-};
+    constructor(prisma: PrismaClient) {
 
-export const protectRouter: ProtectRouter = async (request: FastifyRequest, reply: FastifyReply, next: DoneFuncWithErrOrRes): Promise<void> => {
+        this.prisma = prisma;
 
-    try {
+    };
 
-        const token: string | undefined = request.cookies.jwt;
+    public protect: ProtectRouter = async (
 
-        if (!token) {
+        request: FastifyRequest,
+        reply: FastifyReply,
+        next: DoneFuncWithErrOrRes
 
-            return reply.status(StatusCodes.UNAUTHORIZED).send({
+    ): Promise<FastifyReply | void> => {
 
-                message: "Unauthorized - No Token Provided"
+        try {
+
+            if (!this.jwtSecret) throw new Error('Ambiente variable "JWT_SECRET" is not set');
+
+            if (!request.cookies.jwt) {
+
+                console.log("Unauthorized - No Token Provided");
+
+                return reply.status(StatusCodes.UNAUTHORIZED).send({
+
+                    error: "Unauthorized - No Token Provided",
+                    message: ReasonPhrases.UNAUTHORIZED
+
+                });
+
+            };
+
+            const token: string = request.cookies.jwt;
+            console.log("Received token: ", token);
+
+            const decodedToken: JwtPayload = jwt.verify(token, this.jwtSecret) as JwtPayload;
+
+            if (!decodedToken) {
+
+                return reply.status(StatusCodes.UNAUTHORIZED).send({
+
+                    error: "Unauthorized - Invaled Token Provided",
+                    message: ReasonPhrases.UNAUTHORIZED
+
+                });
+
+            };
+
+            console.log("User id: ", decodedToken.userId);
+
+            const user: UserProtectRouter | null = await this.prisma.user
+                .findUnique({
+
+                    where: {
+
+                        id: decodedToken.userId,
+
+                    },
+                    omit: {
+
+                        hashedPassword: true
+
+                    }
+
+                });
+
+
+            if (!user) {
+
+                console.log("User was not authenticated");
+
+                return reply.status(StatusCodes.FORBIDDEN).send({
+
+                    error: {
+
+                        error: "User was not authenticated",
+                        message: ReasonPhrases.FORBIDDEN
+
+                    }
+
+                });
+
+            };
+
+            console.log("User found w/ token: ", user);
+
+            request.user = user;
+
+            return next();
+
+        } catch (error: unknown) {
+
+            if (error instanceof PrismaClientKnownRequestError && error.code === "P2025") {
+
+                return reply.status(StatusCodes.NOT_FOUND).send({
+
+                    error: {
+
+                        error: "User not found",
+                        message: ReasonPhrases.NOT_FOUND
+
+                    }
+
+                });
+
+            };
+
+            console.error({
+
+                message: "Error in ProtectRouter: ",
+                error: error
 
             });
 
-        };
+            return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
 
-        const decodedToken: JwtPayload = jwt.verify(token, jwtSecret) as JwtPayload;
+                error: {
 
-        if (!decodedToken) {
-
-            return reply.status(StatusCodes.UNAUTHORIZED).send({
-
-                message: "Unauthorized - Invaled Token Provided"
-
-            });
-
-        };
-
-        const user: UserProtectRouter = await prisma
-            .user
-            .findUniqueOrThrow({
-
-                where: {
-
-                    id: decodedToken.userId
+                    default: "Error in ProtectRouter",
+                    message: ReasonPhrases.INTERNAL_SERVER_ERROR
 
                 },
 
-                omit: {
-
-                    hashedPassword: true
-
-                }
-
             });
-
-        request.user = user;
-
-        return next();
-
-    } catch (error: unknown) {
-
-        if (error instanceof PrismaClientKnownRequestError && error.code === 'P2025') {
-
-            console.error("User not found");
-
-            return reply.status(StatusCodes.NOT_FOUND).send({
-
-                errors: {
-
-                    error: "User not found",
-                    message: ReasonPhrases.NOT_FOUND
-
-                }
-
-            });
-
 
         };
 
-        console.error({
-
-            message: "Error in ProtectRouter: ",
-            error: error
-
-        });
-
-        return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-
-            errors: {
-
-                default: "Error in ProtectRouter",
-                message: ReasonPhrases.INTERNAL_SERVER_ERROR
-
-            },
-
-        });
 
     };
 
 };
+
