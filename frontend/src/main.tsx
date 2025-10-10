3571e4bdf702d5909484e837c964158d9372a619
alienaudo <contatotiago819@gmail.com>
2025-10-10 09:28:44 -0400

    Add pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { LoginPage } from './pages/Login/LoginPage';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { SignupPage } from './pages/Signup/SignupPage';
+import { Toaster } from 'react-hot-toast';
+
+const router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <><h1>OK</h1></>
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />,
+    }
+
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+    <StrictMode>
+
+        <RouterProvider router={router} />
+
+        <Toaster />
+
+    </StrictMode>
+
+);
